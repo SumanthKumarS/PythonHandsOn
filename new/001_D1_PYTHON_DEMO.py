094fa8dd824ac9588834db1d043caead4660c762
SumanthKumarS <70691643+SumanthKumarS@users.noreply.github.com>
2023-01-26 10:14:29 +0530

    Add files via upload

---
+++ new/001_D1_PYTHON_DEMO.py
@@ -0,0 +1,4 @@
+print('Hello world')
+print('Hello Cognizant')
+print('Hai Sumanth, Welcome to world of python')
+print("Hello {0!r} and {0!s}".format('foo', 'bin'))
---
+++ new/p1.py
@@ -0,0 +1,9 @@
+
+
+myList = ["Red", "Green", "Blue"]
+print(len(myList))
+myList.append("Purple")
+print(myList)
+# myList = ["Red", "Green", "Blue"]
+myList.reverse()
+print(myList)
